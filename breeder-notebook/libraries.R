8028af8daf0fabae87238754048c0678eee203b0
Matt Wiese <matthewwiese@users.noreply.github.com>
2023-07-11 12:24:13 -0400

    R package update, disable Actions caching

--- breeder-notebook/libraries.R
+++ breeder-notebook/libraries.R
@@ -1,14 +1,11 @@
-# TODO: `destdir = "/tmp/R"`
-install.packages("QBMS", dependencies = TRUE, repos = "http://cran.us.r-project.org")
-
 # CRAN
 devtools::install_version("sommer",      version = "4.3.1",   dependencies = TRUE, repos = "http://cran.us.r-project.org")
 devtools::install_version("visNetwork",  version = "2.1.2",   dependencies = TRUE, repos = "http://cran.us.r-project.org")
 devtools::install_version("plotly",      version = "4.10.0",  dependencies = TRUE, repos = "http://cran.us.r-project.org")
 devtools::install_version("BiocManager", version = "1.30.21", dependencies = TRUE, repos = "http://cran.us.r-project.org")
+devtools::install_version("QBMS",        version = "0.9.1",   dependencies = TRUE, repos = "http://cran.us.r-project.org")
 
 # Bioconductor
-BiocManager::install(version = "3.17")
 BiocManager::install("SummarizedExperiment")
 BiocManager::install("ggtree")
 
