4240964a82c56e171375ba9ef413f7d8611046d8
Matt Wiese <matthewwiese@users.noreply.github.com>
2023-07-13 14:04:39 -0400

    Add async packages required for QBMS
    Source: https://github.com/icarda-git/QBMS/tree/master#troubleshooting-the-installation

--- breeder-notebook/libraries.R
+++ breeder-notebook/libraries.R
@@ -13,6 +13,8 @@ BiocManager::install("ggtree")
 remotes::install_github("maize-genetics/rtassel",    dependencies = TRUE)
 remotes::install_github("maize-genetics/rphg",       dependencies = TRUE)
 remotes::install_github("btmonier/r_flapjack_bytes", dependencies = TRUE)
+remotes::install_github("r-lib/async",               dependencies = TRUE)
+remotes::install_github("icarda-git/QBMS",           dependencies = TRUE)
 
 # Link R kernel with Jupyter
 IRkernel::installspec()
